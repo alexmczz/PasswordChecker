30304ebb4f36d219a43246e085e202d51d752a74
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-04-06 11:00:25 -0700

    Add files via upload

---
+++ password_checker.ps1
@@ -0,0 +1,13 @@
+﻿$check = $true
+While ($check){
+    write-host "Please enter password" -ForegroundColor Yellow
+    $check_user_str = Read-Host -AsSecureString
+    $check_comp = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($check_user_str))
+    if ($check_user_str.Length -ge 7){
+        Write-Host "Password Passes" -ForegroundColor Green
+        Write-Host $check_comp -ForegroundColor DarkGray
+        $check = $false
+    }else{
+        Write-Host "Password Needs to be longer(Atleast 8 Characters)" -ForegroundColor Red
+    }
+}

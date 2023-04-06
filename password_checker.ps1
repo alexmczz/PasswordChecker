f4064a228c2e5d331cf752d2a3a41f8bcd94a919
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-04-06 14:13:45 -0400

    Update password_checker.ps1

--- password_checker.ps1
+++ password_checker.ps1
@@ -5,7 +5,7 @@ While ($check){
     $check_comp = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($check_user_str))
     if ($check_user_str.Length -ge 7){
         Write-Host "Password Passes" -ForegroundColor Green
-        Write-Host $check_comp -ForegroundColor DarkGray
+        Write-Host $check_comp -ForegroundColor DarkGray -BackgroundColor Darkgray
         $check = $false
     }else{
         Write-Host "Password Needs to be longer(Atleast 8 Characters)" -ForegroundColor Red
